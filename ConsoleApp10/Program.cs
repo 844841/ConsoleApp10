3cfd9672fdc4a7a6316a2519ef2360e155ddf95a
15341a1205 <41323544+15341a1205@users.noreply.github.com>
2020-02-20 14:51:21 +0530

    Initial commit

---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace getset
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             private string name;
+             public string Name
+             {
+              get { return name; }
+              set { set name; }
+
+             }
+           
+        }
+    }
+}
